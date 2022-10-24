022ef3c80a9e4f9e847d336e7deee4509944149e
Kevin Sasse <sassek70@gmail.com>
2022-10-24 14:48:01 -0400

    completed new player form

---
+++ qb-stat-tracker/src/components/NewQBForm.js
@@ -0,0 +1,43 @@
+import { useState } from "react"
+
+
+
+const NewQbForm = ({ onFormSubmit }) => {
+    const [formData, setFormData] = useState({
+        name: "Josh Allen",
+        team: "Buffalo Bills",
+        yards: 0,
+        rtouchdowns:0,
+        ptouchdowns:0,
+        completions:0,
+        image:"./images/stockplayer.jpg",
+        favorited: false
+    })
+
+    const handleChange = (event) => {
+        const { name, value } = event.target
+        setFormData((formData) => ({...formData, [name]: value}))
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        onFormSubmit(formData)        
+    }
+
+    console.log(formData)
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" name="name" placeholder="Player's Name" onChange={handleChange}/>
+            <input type="text" name="team" placeholder="Player's Team" onChange={handleChange}/>
+            <input type="number" name="yards" placeholder="Total Yards" onChange={handleChange}/>
+            <input type="number" name="rtouchdowns" placeholder="Passing Touchdowns" onChange={handleChange}/>
+            <input type="number" name="ptouchdowns" placeholder="Rushing Touchdowns" onChange={handleChange}/>
+            <input type="number" name="completions" placeholder="Completions Touchdowns" onChange={handleChange}/>
+            <input type="text" name="image" placeholder="Player's image" onChange={handleChange}/>
+            <button>Add Player</button>
+        </form>
+    )
+}
+
+export default NewQbForm
